b1fde4f6f0a5d2d14f51588dd0024483b6983cfe
mohsinakmal <mohsinakmal210@gmail.com>
2021-01-26 18:38:37 +0500

    1/26/2021

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        return Future.error(
+            'Location permissions are denied (actual value: $permission).');
+      }
+    }
+    Position position = await Geolocator.
+    getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    return position;
+  }
+
+  void getLocation() async{
+    Position position = await Geolocator.
+    getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //Get the current location
+            _determinePosition();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
